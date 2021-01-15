76e04364c9ab4a4382eae75b54f8649139b379c2
Vanshika-14 <vanshikakonanki14@gmail.com>
2021-01-15 15:57:03 +0530

    Changed Files!

--- sketch.js
+++ sketch.js
@@ -1,29 +1,53 @@
+
 var ball;
 
+var database;
+
+var position;
+
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    ballposition = database.ref('ball/positions');
+    ballposition.on("value", readposition, showerror);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+//Reading The Values
+function readposition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+//Showing Errors
+function showerror(){
+    console.log("Error While Reading Or Writing Data.");
+}
+
+//Writing The Positions
+function writePosition(x, y){
+    database.ref('ball/positions').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
 }
